750412bf0c30be462d99ca7b460e118d0dd6834e
arek <arysom@interia.pl>
2019-03-16 00:29:48 +0100

    docblock fomatting

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Thing;
@@ -10,55 +9,79 @@ use Carbon\Carbon;
 class ReservationController extends Controller
 {
 
-    public function insert($thing_id, Request $request)
+    /**
+     * Insert
+     *
+     * @param  int $thing_id
+     * @param  Request $request
+     * @return response
+     */
+    public function insert(int $thing_id, Request $request)
     {
         $start = Carbon::parse($request->input('start'));
         $end = Carbon::parse($request->input('end'));
-        if($end < $start) return response('End earlier than start', 409);
+        if ($end < $start) {
+            return response('End earlier than start', 409);
+        }
         $thing = Thing::with('reservations')->findOrFail($thing_id);
         //check the existing reservations
-        if($thing->reservations->count() > 0) {
+        if ($thing->reservations->count() > 0) {
             foreach ($thing->reservations as $value) {
                 //if start inside
-                if($start >= $value->start && $start <= $value->end) return response('Start overlapping', 409);
+                if ($start >= $value->start && $start <= $value->end) {
+                    return response('Start overlapping', 409);
+                }
                 //if end inside
-                if($end >= $value->start && $end <= $value->end) return response('End overlapping', 409);
+                if ($end >= $value->start && $end <= $value->end) {
+                    return response('End overlapping', 409);
+                }
             }
         }
 
         //add the reservation
-        $reservation = Reservation::create([
-            'user_id' => Auth::user()->id,
-            'thing_id'=>$thing_id,
-            'start' => $start,
-            'end' => $end,
-            'status' => 'proposed'
-        ]);
-        return response()->json(['status'=>'ok', 'reservation' => $reservation], 201);
+        $reservation = Reservation::create(
+            [
+                'user_id' => Auth::user()->id,
+                'thing_id'=>$thing_id,
+                'start' => $start,
+                'end' => $end,
+                'status' => 'proposed'
+            ]
+        );
+        return response()->json(
+            ['status'=>'ok', 'reservation' => $reservation], 201
+        );
     }
 
-    public function update($id, Request $request)
+    /**
+     * Update
+     *
+     * @param  int $id
+     * @param  Request $request
+     * @return response
+     */
+    public function update(int $id, Request $request)
     {
         $reservation = Reservation::with('thing')->find($id);
         $status = $request->input('status');
+
         //if user_id === reservations.user_id
         //can cancel
-        if(
-            Auth::user()->id === $reservation->user_id &&
-            $status !== 'canceled'
-        ) return response('once reservation done, user can only cancel', 403);
+        if (Auth::user()->id === $reservation->user_id && $status !== 'canceled') {
+            return response('once reservation done, user can only cancel', 403);
+        }
 
-        if(
-            Auth::user()->id === $reservation->user_id &&
-            $status !== 'canceled'
-        ) return response('once reservation done, user can only cancel', 403);
+        if (Auth::user()->id === $reservation->user_id && $status !== 'canceled') {
+            return response('once reservation done, user can only cancel', 403);
+        }
 
         //if user_id === thing.user_id
         //can accept or reject
-        if (
-            Auth::user()->id === $reservation->thing->id &&
-            ($status === 'proposed' || $status === 'canceled')
-        ) return response('you can only accept or reject', 403);
+        if (Auth::user()->id === $reservation->thing->id &&
+           ($status === 'proposed' || $status === 'canceled')
+        ) {
+            return response('you can only accept or reject', 403);
+        }
 
         $reservation->status = $status;
         $reservation->save();
--- app/Http/Controllers/ThingController.php
+++ app/Http/Controllers/ThingController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Thing;
@@ -8,63 +7,102 @@ use Auth;
 class ThingController extends Controller
 {
 
+    /**
+     * Index
+     *
+     * @return response
+     */
     public function index()
     {
         $things = Thing::paginate();
-        if($things->count() === 0) return response()->json(['message'=> 'no things yet'], 404);
-        return response()->json(['things'=> $things],200);
+        if ($things->count() === 0) {
+            return response()->json(['message'=> 'no things yet'], 404);
+        }
+        return response()->json(['things'=> $things], 200);
     }
 
+    /**
+     * Insert
+     *
+     * @param  Request $request
+     * @return response
+     */
     public function insert(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required'
-        ]);
+        $this->validate(
+            $request, [
+                'name' => 'required'
+            ]
+        );
 
         if ($request->hasFile('photo') && $request->file('photo')->isValid()) {
             $ext = $request->file('photo')->guessExtension();
             $filename = md5(time()).'.'.$ext;
-            $request->file('photo')->move(storage_path('app/images_things'), $filename);
+            $request->file('photo')
+                ->move(storage_path('app/images_things'), $filename);
         }
-        $thing = Thing::create([
-            'name' => $request->input('name'),
-            'user_id' => Auth::user()->id,
-            'desc' => $request->input('desc'),
-            'filename' => !empty($filename)?$filename:null
-        ]);
+        $thing = Thing::create(
+            [
+                'name' => $request->input('name'),
+                'user_id' => Auth::user()->id,
+                'desc' => $request->input('desc'),
+                'filename' => !empty($filename)?$filename:null
+            ]
+        );
 
         return response()->json(['status'=>'ok', 'thing' => $thing], 201);
     }
 
-    public function get($id)
+    /**
+     * Get
+     *
+     * @param  int $id
+     * @return response
+     */
+    public function get(int $id)
     {
         $thing = Thing::find($id);
 
-        if($thing===null) return response()->json(['status'=>'bad'], 404);
+        if ($thing===null) {
+            return response()->json(['status'=>'bad'], 404);
+        }
 
         return response()->json(['status'=>'ok', 'thing'=>$thing], 200);
-    }
+    } // End function get
 
-    public function update($id, Request $request)
+    /**
+     * Update
+     *
+     * @param  int $id
+     * @param  Request $request
+     * @return response
+     */
+    public function update(int $id, Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required'
-        ]);
+        $this->validate(
+            $request, [
+                'name' => 'required'
+            ]
+        );
 
         $thing = Thing::find($id);
 
         //check if owner
-        if (Auth::user()->id !== $thing->user_id) return response()->json(['message' => 'not an owner of the thing'], 401);
-
+        if (Auth::user()->id !== $thing->user_id) {
+            return response()->json(['message' => 'not an owner of the thing'], 401);
+        }
         $thing->name = $request->input('name');
         $thing->desc = $request->input('desc');
 
         if ($request->hasFile('photo') && $request->file('photo')->isValid()) {
             //delete existing photo
-            if($thing->filename !== null) unlink(storage_path('app/images_things').$thing->filename);
+            if ($thing->filename !== null) {
+                unlink(storage_path('app/images_things').$thing->filename);
+            }
             $ext = $request->file('photo')->guessExtension();
             $filename = md5(time()).'.'.$ext;
-            $request->file('photo')->move(storage_path('app/images_things'), $filename);
+            $request->file('photo')
+                ->move(storage_path('app/images_things'), $filename);
             $thing->filename = $filename;
         }
 
@@ -73,7 +111,13 @@ class ThingController extends Controller
         return response()->json(['status'=>'ok', 'thing' => $thing], 200);
     }
 
-    public function delete($id)
+    /**
+     * Delete
+     *
+     * @param  int $id
+     * @return response
+     */
+    public function delete(int $id)
     {
         Thing::findOrFail($id)->delete();
         return response('Deleted successfully', 200);
