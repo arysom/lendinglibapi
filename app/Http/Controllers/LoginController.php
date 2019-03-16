8ac1634728bf23ce8ad45c298dedd050ee9a57e7
arek <arysom@interia.pl>
2019-03-16 16:19:53 +0100

    cleaning job

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,30 +7,31 @@ use Illuminate\Support\Facades\Hash;
 class LoginController extends Controller
 {
     /**
-     * Create a new controller instance.
+     * Login
      *
-     * @return void
+     * @param  Request $request
+     * @return response
      */
-    public function __construct()
-    {
-        //
-    }
-
     public function login(Request $request)
     {
-        $this->validate($request, [
-            'email' => 'required',
-            'password' => 'required'
-        ]);
+        $this->validate(
+            $request,
+            [
+                'email' => 'required',
+                'password' => 'required'
+            ]
+        );
 
         $user = User::where('email', $request->input('email'))->first();
-        if(Hash::check($request->input('password'), $user->password)){
+        if (Hash::check($request->input('password'), $user->password)) {
             $api_token = base64_encode(str_random(40));
-            User::where('email', $request->input('email'))->update(['api_token' => "$api_token"]);;
-            return response()->json(['status' => 'success','api_token' => $api_token]);
+            User::where('email', $request->input('email'))
+                ->update(['api_token' => "$api_token"]);
+            return response()->json(
+                ['status' => 'success','api_token' => $api_token]
+            );
         } else {
-            return response()->json(['status' => 'fail'],401);
+            return response()->json(['status' => 'fail'], 401);
         }
     }
-    //
 }
--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,22 +5,31 @@ use App\User;
 
 class RegisterController extends Controller
 {
+    /**
+     * Insert
+     *
+     * @param Request $request
+     * @return response
+     */
     public function insert(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required|unique:users',
-            'email' => 'required|email|unique:users',
-            'password' => 'required'
-        ]);
+        $this->validate(
+            $request,
+            [
+                'name' => 'required|unique:users',
+                'email' => 'required|email|unique:users',
+                'password' => 'required'
+            ]
+        );
 
-        $user = User::create([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'password' => app('hash')->make($request->input('password'))
-        ]);
+        $user = User::create(
+            [
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => app('hash')->make($request->input('password'))
+            ]
+        );
 
         return response('the user has been created', 201);
     }
-
-    //
 }
